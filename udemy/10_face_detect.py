75195744ff925c83691ccdbd5e5c7b49f6167142
Bengsiswanto <bengsiswantoh@gmail.com>
2021-03-16 13:53:58 +0700

    add faces_train

--- udemy/10_face_detect.py
+++ udemy/10_face_detect.py
@@ -1,6 +1,14 @@
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_cascade = cv.CascadeClassifier(
+    "cascades/haarcascade_frontalface_default.xml")
 
 img = cv.imread("images/chara_16.png")
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+face_detect = face_cascade.detectMultiScale(gray_img, 1.1, 4)
+
+for(x, y, w, h) in face_detect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
+
+cv.imshow("image", img)
+cv.waitKey(0)
